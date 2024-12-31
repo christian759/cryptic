de79888fedeafc9242fe7a5e242efefe63c8d29e
ceo1 <christian4onos@outlook.com>
2024-12-31 20:24:12 +0100

    amends

--- app/src/main/java/com/ceo1/cryptic/blocks/extract_text.kt
+++ app/src/main/java/com/ceo1/cryptic/blocks/extract_text.kt
@@ -8,7 +8,23 @@ import com.ceo1.cryptic.Theme
 fun extractText(primaryTheme: Theme){
     val context = LocalContext.current
 
-    //
-
+    Column{
+        Button(colors = ButtonColors(containerColor = primaryTheme.color3, contentColor = primaryTheme.color1, disabledContentColor = Color.Unspecified,
+            disabledContainerColor = Color.Unspecified),
+            border = BorderStroke(5.dp, primaryTheme.color1),
+            shape = RoundedCornerShape(12.dp),
+            modifier = Modifier
+                .weight(0.5f)
+                .padding(5.dp)
+            .fillMaxHeight(),
+            onClick = {
+                // extract image
+            }){ Text("Extract Image", fontSize = TextUnit(18f, TextUnitType.Sp))}
+    }
+    Spacer(modifier = Modifier.padding(10.dp))
     Text("extract text")
+
+    BasicText(
+        //content =  image
+    )
 }
